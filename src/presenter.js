40eb00d8fbb877b2cde7b3ec88d1a13b456ad199
jarPunk <jar_ax@hotmail.com>
2025-03-04 21:44:31 -0400

    modificacion de presenter

--- src/presenter.js
+++ src/presenter.js
@@ -1,15 +1,18 @@
-import sumar from "./sumador";
+import convertirARomano from "./romanos.js";
+const numeroRomano = document.querySelector("#numero-romano");
+const formRomano = document.querySelector("#romanos-form");
+const romanoDiv = document.querySelector("#romano-div");
 
-const first = document.querySelector("#primer-numero");
-const second = document.querySelector("#segundo-numero");
-const form = document.querySelector("#sumar-form");
-const div = document.querySelector("#resultado-div");
-
-form.addEventListener("submit", (event) => {
+formRomano.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  const firstNumber = Number.parseInt(first.value);
-  const secondNumber = Number.parseInt(second.value);
+  const number = Number.parseInt(numeroRomano.value);
 
-  div.innerHTML = "<p>" + sumar(firstNumber, secondNumber) + "</p>";
+  if (isNaN(number) || number < 1 || number > 500) {
+    romanoDiv.innerHTML = "<p style='color:red;'>Por favor, introduce un número entre 1 y 500.</p>";
+    return;
+  }
+
+  const resultado = convertirARomano(number);
+  romanoDiv.innerHTML = "<p>Número en romano: " + resultado + "</p>";
 });
