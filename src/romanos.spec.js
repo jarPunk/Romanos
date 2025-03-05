f8ebe12ebfa3f8dde539baeb78fb8097f38e5f62
jarPunk <jar_ax@hotmail.com>
2025-03-04 21:01:25 -0400

    prueba convertir 2

--- src/romanos.spec.js
+++ src/romanos.spec.js
@@ -4,5 +4,8 @@ describe("convertirARomano", () => {
   it("debería convertir 1 a I", () => {
     expect(convertirARomano(1)).toEqual("I");
   });
+  it("debería convertir 2 a II", () => {
+    expect(convertirARomano(2)).toEqual("II");
+  });
 
 });
